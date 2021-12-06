7770cb831aeb34bf887304dbd9835e31e58a6be2
jm <jm@pronexo.com>
2021-12-06 14:16:47 -0300

    15.0

--- odoo_restart/__manifest__.py
+++ odoo_restart/__manifest__.py
@@ -14,6 +14,8 @@
     'author': "Pronexo",
     'website': "https://www.pronexo.com",
     'category': 'Extra Tools',
+    'license': 'AGPL-3',
+    'price': 15,
     'version': '15.0.1.0.0',
     'depends': ['base'],
     'external_dependencies': {'python' : ["pexpect"]},
