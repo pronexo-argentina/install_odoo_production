634d9b184eb04d2516045f81361f12e7aa628abe
Pronexo.com <pronexo@gmail.com>
2022-02-23 19:53:39 -0300

    Update odoo13.sh

--- install_odoo_production/odoo13.sh
+++ install_odoo_production/odoo13.sh
@@ -60,7 +60,10 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install -y git
 # Update and install Postgresql
-sudo apt-get install postgresql -y
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install postgresql-14 -y
 sudo su - postgres -c "createuser -s $usuario"
 
 sudo mkdir $PATHBASE
