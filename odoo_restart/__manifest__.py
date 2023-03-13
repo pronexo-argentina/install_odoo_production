e7a73ddae412f084b23bc14df9d10ed8d56bb157
Pronexo.com <pronexo@gmail.com>
2023-03-13 15:23:04 -0300

    Update __manifest__.py

--- odoo_restart/__manifest__.py
+++ odoo_restart/__manifest__.py
@@ -16,7 +16,7 @@
     'category': 'Extra Tools',
     'license': 'AGPL-3',
     'price': 15,
-    'version': '15.0.1.0.0',
+    'version': '16.0.1.0.0',
     'depends': ['base'],
     'external_dependencies': {'python' : ["pexpect"]},
     'data': [
